424d71ddfa60f886358537301f6e938a29eb3804
Edmund Edgar <ed@realitykeys.com>
2017-05-31 16:43:50 +0900

    docs, truffle install script

---
+++ setup-truffle.sh
@@ -0,0 +1,7 @@
+# http://www.techtonet.com/how-to-install-and-execute-truffle-on-an-ubuntu-16-04/
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g express
+sudo npm install -g truffle
+
